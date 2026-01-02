6245d98861e711c04828bf2fa3295b338715a5f6
Projectius_student <sprojectiustech@gmail.com>
2026-01-02 22:27:48 +0300

    send logic

--- tasks/konstantinov_s_broadcast/mpi/src/ops_mpi.cpp
+++ tasks/konstantinov_s_broadcast/mpi/src/ops_mpi.cpp
@@ -48,12 +48,41 @@ bool KonstantinovSBroadcastMPI<T>::RunImpl() {
   int tree_rank = (prank - root_prank + pcount) % pcount;
 
   // родитель в дереве
-  int parent_tree_rank = (tree_rank - 1) / k;
+  int parent_tree_rank = (tree_rank == 0) ? -1 : (tree_rank - 1) / k;
   int parent_prank = (parent_tree_rank < 0)
       ? MPI_ANY_SOURCE
       : (parent_tree_rank + root_prank) % pcount;
 
+  // приём данных от родителя
+  if (tree_rank != 0) {
+    MPI_Recv(data, elem_count, mpi_type,
+             parent_prank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
 
+  // вычисляем потомков
+  std::vector<int> child_pranks;
+  for (int i = 1; i <= k; ++i) {
+    int child_tree_rank = (k * tree_rank) + i;
+    if (child_tree_rank < pcount) {
+      int child_prank = (child_tree_rank + root_prank) % pcount;
+      child_pranks.push_back(child_prank);
+    }
+  }
+
+  // неблокирующая рассылка детям
+  std::vector<MPI_Request> send_requests(child_pranks.size());
+  for (std::size_t i = 0; i < child_pranks.size(); ++i) {
+    MPI_Isend(data, elem_count, mpi_type,
+              child_pranks[i], 0, MPI_COMM_WORLD,
+              &send_requests[i]);
+  }
+
+  if (!send_requests.empty()) {
+    MPI_Waitall(static_cast<int>(send_requests.size()),
+                send_requests.data(), MPI_STATUSES_IGNORE);
+  }
+
+  this->GetOutput() = buffer;
   return true;
 }
 
